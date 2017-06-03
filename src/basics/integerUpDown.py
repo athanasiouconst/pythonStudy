047f26399cd167d1bcfc92e1399a4479fc8d2fff
athanasiouconst <csiouath@gmail.com>
2017-06-03 21:02:58 +0300

    prime numbers

---
+++ src/basics/integerUpDown.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+#input of protosArithmos value
+protosArithmos = ""
+protosArithmosBoolean = False
+
+#input of deyterosArithmos value
+deyterosArithmos = ""
+deyterosArithmosBoolean = False
+
+
+while( type(protosArithmos)!=int ):
+    protosArithmos = int(input("Παρακαλώ εισάγετε τον πρώτο ακέραιο αριθμό της επιθυμίας σας : "))
+    if(type(protosArithmos)==int):
+        protosArithmosBoolean = True
+        break
+
+while( type(deyterosArithmos)!=int ):
+    deyterosArithmos = int(input("Παρακαλώ εισάγετε τον δεύτερο ακέραιο αριθμό της επιθυμίας σας : "))
+    if(type(deyterosArithmos)==int):
+        deyterosArithmosBoolean = True
+        break
+
+if(protosArithmosBoolean == False & deyterosArithmosBoolean == False):
+    print("Try Again!")
+
+
+if(protosArithmosBoolean == True & deyterosArithmosBoolean == True):
+    if(deyterosArithmos < protosArithmos):
+        temp = protosArithmos
+        protosArithmos = deyterosArithmos
+        deyterosArithmos=temp
+    
+    for protosArithmos in range(protosArithmos, deyterosArithmos):
+        if protosArithmos%2 != 0:
+            print(protosArithmos)
+            
+
+  
