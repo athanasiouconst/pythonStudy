e2806d96ed6a240a931185c3326fb35e119270d4
athanasiouconst <csiouath@gmail.com>
2017-06-04 15:52:00 +0300

    string modules and basics funcs

---
+++ src/basics/stringVertical.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#input of chars
+stringText=raw_input("Παρακαλώ εισάγετε μια πρόταση : ")
+
+#length of the string 
+stringLength = len(stringText)
+
+#use upper func for uppercasing characters
+#and use loop for printing vertical them
+for i in range(0, stringLength,1):
+    print(stringText[i].upper())
+
